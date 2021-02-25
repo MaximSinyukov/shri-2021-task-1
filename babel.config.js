b2ea011b2363d1fc94b4a8ec41ed3f75178a837a
Maxim Sinyukov <verero1173@yandex.ru>
2021-02-25 21:11:52 +0300

    feat: создать структуру проекта и настроить вебпак

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const presets = [
+  ['@babel/env', {
+    targets: {
+      firefox: '85',
+      chrome: '88',
+      ios: '14'
+    },
+
+    useBuiltIns: "entry"
+  }]
+];
+
+module.exports = { presets };
